fabb3368c7ed273ed609a7dc8f4db72348152fa4
Elysa Anggun L <39848012+anggunelysal@users.noreply.github.com>
2019-02-28 13:19:31 +0700

    Create structure_directory.py

---
+++ structure_directory.py
@@ -0,0 +1,34 @@
+.
+├── annotations
+│   ├── test
+│   │   ├── IMG-20180108-WA0115.xml
+│   │   ├── ...
+│   │   ├── IMG-20180108-WA0120.xml
+│   │   └── IMG-20180108-WA0121.xml
+│   └── train
+│       ├── 100.E 6467 QW-09-20.xml
+│       ├── ...
+│       ├── 255.E 6527 OD-02-20.xml
+│       └── 256.E 4572 SS-01-16.xml
+├── data
+│   ├── plate_number_map.pbtxt
+│   ├── test.record
+│   ├── test_labels.csv
+│   ├── train.record
+│   └── train_labels.csv
+├── images
+│   ├── test
+│   │   ├── IMG-20180108-WA0115.jpeg
+│   │   ├── ...
+│   │   ├── IMG-20180108-WA0120.jpg
+│   │   └── IMG-20180108-WA0121.jpg
+│   └── train
+│       ├── 100.E 6467 QW-09-20.jpeg
+│       ├── ...
+│       ├── 255.E 6527 OD-02-20.jpg
+│       └── 256.E 4572 SS-01-16.jpg
+├── training
+│   └── plate_number_v1.config
+├── generate_tfrecord.py
+└── xml_to_csv.py
+8 directories, 1016 files
